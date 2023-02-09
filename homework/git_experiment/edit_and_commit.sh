b1c58e0aef33a59421c5f8875d79c3ec10edcd54
Weishan Li <wli51@jh.edu>
2023-02-08 23:45:29 -0500

    Add first sentence to Tex file

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -2,6 +2,13 @@
 
 for commit_count in {2..100}
 do
-  let linenum="10+$commit_count"
+	let linenum="10+$commit_count"
+	module load tex
+	sed -i "$linenum i adding one line." foo.tex
+	pdflatex foo.tex > /dev/null 2>&1
+	
+	git add -u
+	git commit -m "Add a line to Tex file."
+
   # Fill in
 done
