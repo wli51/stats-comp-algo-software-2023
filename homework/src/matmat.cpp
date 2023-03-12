27873cddba36c456675e1aca4f66543c213fcde5
wli51 <wli51@jh.edu>
2023-03-12 04:00:21 -0400

    rcpp optimimized computation

--- homework/src/matmat.cpp
+++ homework/src/matmat.cpp
@@ -14,6 +14,15 @@ NumericMatrix row_col_dot_matmat(
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
   // Fill in; remember to use (,) instead of [,] for accessing matrix elements
+  for (int i = 0; i < n_row_out; ++i) {
+    for (int j = 0; j < n_col_out; ++j) {
+      double sum = 0; 
+      for (int k = 0; k < A.ncol(); ++k) {
+         sum += A(i,k) * B(k, j);
+      }
+      result(i, j) = sum;
+    }
+  }
   return result;
 }
 
@@ -27,9 +36,14 @@ NumericMatrix col_oriented_matmat(
   int n_row_out = A.nrow();
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
-  for (int j = 0; j < n_col_out; ++j) {
-    // Calculate result[, j] = A %*% B[, j] in column-oriented manner for each j
-    // Fill in
+  
+  for (int k = 0; k < n_col_out; ++k) {
+    for (int j = 0; j < A.ncol(); ++j) {
+      for (int i = 0; i < A.nrow(); ++i) {
+        result(i, k) += A(i, j) * B(j, k);
+      }
+    }
   }
+  
   return result;
 }
